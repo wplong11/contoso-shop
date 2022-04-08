e6a5144be549cf6bc49855e9da8b1e45d2ad30d4
Donghyeon Kim <wplong11@users.noreply.github.com>
2022-04-08 18:48:52 +0900

    상품 수정 API 끝점 추가

--- source/Inventory/core/src/test/kotlin/com/contoso/inventory/application/controller/ProductControllerTest.kt
+++ source/Inventory/core/src/test/kotlin/com/contoso/inventory/application/controller/ProductControllerTest.kt
@@ -2,17 +2,17 @@ package com.contoso.inventory.application.controller
 
 import com.contoso.inventory.FeedDto
 import com.contoso.inventory.ProductDto
+import com.contoso.inventory.command.UpdateProductCommand
 import com.contoso.inventory.defaultObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @WebMvcTest(controllers = [ProductController::class])
@@ -31,5 +31,20 @@ class ProductControllerTest @Autowired constructor(
         val responseText: String = response.andReturn().response.contentAsString
         val actual: FeedDto<ProductDto> = defaultObjectMapper.readValue(responseText)
         assertThat(actual.items).isNotEmpty
+    }
+
+    @Test
+    fun `sut executes UpdateProductCommand correctly`() {
+        // Act
+        val content = UpdateProductCommand.getExamples().random()
+        val response = mockMvc.perform(
+            post("/products/execute/update-product-command")
+                .content(defaultObjectMapper.writeValueAsString(content))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+
+        // Assert
+        response.andExpect(status().isOk)
     }
 }

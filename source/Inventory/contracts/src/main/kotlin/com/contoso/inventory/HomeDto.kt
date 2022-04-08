b863f1ac58aeb52389e8f7998bc8be80a3de8d4e
Donghyeon Kim <wplong11@users.noreply.github.com>
2022-04-08 17:47:17 +0900

    안 쓰는 API 끝점 삭제

--- source/Inventory/contracts/src/main/kotlin/com/contoso/inventory/HomeDto.kt
+++
@@ -1,9 +0,0 @@
-package com.contoso.inventory
-
-import io.swagger.v3.oas.annotations.media.Schema
-
-@Schema(description = "HomeDto 입니다")
-data class HomeDto(
-    @Schema(description = "메세지 내용 입니다")
-    val message: String,
-)
